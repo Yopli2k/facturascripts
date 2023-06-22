3882421a35d1f40888e065b9c9cf359431088bd2
Yopli2k <yopli2000@gmail.com>
2023-06-22 08:54:53 +0200

    Fix accounting error

--- Core/Lib/Accounting/InvoiceToAccounting.php
+++ Core/Lib/Accounting/InvoiceToAccounting.php
@@ -275,13 +275,6 @@ class InvoiceToAccounting extends AccountingClass
             if (false === $done) {
                 return false;
             }
-
-            if ((float)$value['totalrecargo'] != 0.00) {
-                $surcharge = $this->getSurchargeAccount($tax->codsubcuentasop_recargo, $subAccount);
-                if (false === $this->addSurchargeLine($entry, $surcharge, true, (float)$value['totalrecargo'])) {
-                    return false;
-                }
-            }
         }
 
         return true;
